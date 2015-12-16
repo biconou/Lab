74b9f788998f7f1e2f3bd9033007fd9e1c80fd0c
rcocula <remi.cocula@soprasteria.com>
2015-12-16 15:20:30 +0100

    debut test metrics

---
+++ metrics/metrics-tests/src/main/java/org/github/biconou/lab/App.java
@@ -0,0 +1,13 @@
+package org.github.biconou.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
